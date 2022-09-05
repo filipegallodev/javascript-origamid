19d490196678f85c3d43383098dd6dc8098392b1
Filipe <filipegallodev@hotmail.com>
2022-09-05 20:34:42 -0300

    Renomeando pasta

---
+++ aulas/variaveis/script.js
@@ -0,0 +1,11 @@
+var firstName = "Filipe";
+var idade = 22;
+var comidaFavorita;
+comidaFavorita = "Pizza";
+var lastName;
+var altura;
+var cidade;
+var estado;
+var pais;
+
+console.log(nome, idade, comidaFavorita)
