20b053262758a9b81185d4b949c7b2c32c0a8d1a
Filipe <filipegallodev@hotmail.com>
2022-09-06 01:04:28 -0300

    Números e Operadores

---
+++ aulas/numeros-e-operadores/script.js
@@ -0,0 +1,17 @@
+var total = 10 + 5 * 2 / 2 + 20;
+console.log(total);
+
+var divideNome = "Filipe" / 2;
+var divideSobrenome = "Gallo" - 2;
+console.log(divideNome, divideSobrenome);
+
+console.log(+"200" + 50);
+
+var numero = 5;
+numero++;
+console.log(numero);
+
+var numero = +"80" / 2;
+var unidade = "kg";
+var peso = numero + unidade;
+console.log(peso);
