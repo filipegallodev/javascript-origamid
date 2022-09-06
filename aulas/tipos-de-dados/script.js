a52644655f253f813c33c43f0ed766bccf5762a9
Filipe <filipegallodev@hotmail.com>
2022-09-06 00:54:11 -0300

    Tipos de dados

---
+++ aulas/tipos-de-dados/script.js
@@ -0,0 +1,14 @@
+var time = "Palmeiras";
+
+var ano = 2022;
+
+var idade = 22;
+
+var firstName = "Filipe";
+var lastName = "Gallo";
+var nomeCompleto = `${firstName} ${lastName}`;
+var verificarTipoNome = typeof nomeCompleto;
+
+var frase = "It's time";
+
+console.log(verificarTipoNome)
