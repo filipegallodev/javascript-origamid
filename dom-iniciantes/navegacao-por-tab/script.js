524d366ba19f07b83b49c9abfe13ecde33ef8b1a
Filipe <fgallo_dev@hotmail.com>
2022-09-27 09:01:43 -0300

    Navegação por tab

---
+++ dom-iniciantes/navegacao-por-tab/script.js
@@ -0,0 +1,18 @@
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add('ativo');
+
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove('ativo');
+    })
+
+    tabContent[index].classList.add('ativo');
+  }
+
+  tabMenu.forEach((item, index) => {
+    item.addEventListener('click', () => activeTab(index));
+  });
+}
