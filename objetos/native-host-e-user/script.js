5c883a88cf44224df3e22ea1d7c796c27d15eb74
Filipe <fgallo_dev@hotmail.com>
2022-09-29 13:26:33 -0300

    Native, Host e User

---
+++ objetos/native-host-e-user/script.js
@@ -0,0 +1,34 @@
+// console.log(typeof Array);
+// console.log(typeof Array.from);
+// console.log(typeof Array.asdasdasd);
+
+// if (typeof Array.asdasdasd !== 'undefined') {
+//   console.log('Existe');
+// }
+// else {
+//   console.log('Não existe');
+// }
+
+
+
+console.log('- Exercícios -');
+// Liste 5 objetos nativos
+console.log('5 objetos nativos')
+console.log(Object);
+console.log(Function);
+console.log(String);
+console.log(Number);
+console.log(Array);
+
+// Liste 5 objetos do browser
+console.log('5 objetos do browser')
+console.log(HTMLElement);
+console.log(Document);
+console.log(XMLDocument);
+console.log(NodeList);
+console.log(Window);
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+console.log(document.exitPictureInPicture);
+console.log(document.wasDiscarded);
