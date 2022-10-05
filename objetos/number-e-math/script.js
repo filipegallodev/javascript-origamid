b93e3e11a4493767e2317f6b0896d97ba7dcbda8
Filipe <fgallo_dev@hotmail.com>
2022-10-05 14:37:30 -0300

    Number e Math 1

---
+++ objetos/number-e-math/script.js
@@ -0,0 +1,52 @@
+// const numero = 500.25428;
+
+// console.log(Number.isNaN(NaN));
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN("Texto qualquer"));
+// console.log(Number.isInteger(numero));
+// console.log(Number.isInteger(5.2));
+
+// console.log("---");
+// console.log(parseFloat("99.50"));
+// console.log(Number.parseFloat("99.50"));
+// console.log(Number.parseFloat("$ 99.50"));
+// console.log(Number.parseFloat("100.70 reais"));
+// console.log(Number.parseInt("100.75 reais"));
+// console.log(Number.parseInt(100.75));
+
+// console.log(numero.toFixed());
+// console.log(numero.toFixed(2));
+// console.log(+numero.toFixed(4));
+
+// console.log(numero.toString());
+
+// const preco = 59.99;
+
+// console.log(
+//   preco.toLocaleString("en-US", { style: "currency", currency: "USD" })
+// );
+// console.log(
+//   preco.toLocaleString("pt-BR", { style: "currency", currency: "BRL" })
+// );
+// console.log(
+//   preco.toLocaleString("en-GB", { style: "currency", currency: "EUR" })
+// );
+
+// console.log(Math.PI);
+// console.log(Math.LN10);
+
+// console.log(Math.abs(-5.5));
+// console.log(Math.ceil(4.8334));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.8334));
+// console.log(Math.floor(4.3));
+// console.log(Math.round(4.8334));
+// console.log(Math.round(4.3));
+
+// console.log(Math.max(5, 2, 3, 10));
+// console.log(Math.min(5, 2, 3, 10));
+
+// console.log(Math.round(Math.random() * 100));
+
+// Número entre 75 e 100
+// console.log(Math.round(Math.random() * 25) + 75);
