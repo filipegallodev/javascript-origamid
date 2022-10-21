ff2cf1c764defdfd414c32515711481442b0b2f7
Filipe <fgallo_dev@hotmail.com>
2022-10-21 12:40:37 -0300

    Menu Mobile

---
+++ efeitos-no-dom/menu-mobile/js/script.js
@@ -0,0 +1,17 @@
+import { initScrollSuave } from "./modules/scroll-suave.js";
+import { initAnimacaoScroll } from "./modules/scroll-animacao.js";
+import { initAccordion } from "./modules/accordion.js";
+import { initTabNav } from "./modules/tabnav.js";
+import { initModal } from "./modules/modal.js";
+import { initTooltip } from "./modules/tolltip.js";
+import { initDropdownMenu } from "./modules/dropdown-menu.js";
+import { initMenuMobile } from "./modules/menu-mobile.js";
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
