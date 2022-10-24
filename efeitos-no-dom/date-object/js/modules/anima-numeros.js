d80e5e1c9a33edd062c1dfd3c96d2943722451a3
Filipe <fgallo_dev@hotmail.com>
2022-10-24 10:27:53 -0300

    Date Object

---
+++ efeitos-no-dom/date-object/js/modules/anima-numeros.js
@@ -0,0 +1,33 @@
+export function initAnimaNumeros() {
+  function animaNumeros() {
+    const numeros = document.querySelectorAll("[data-numero]");
+
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
+
+      let start = 0;
+      const timer = setInterval(() => {
+        start += incremento;
+        numero.innerText = start;
+
+        if (start > total) {
+          numero.innerText = total;
+          clearInterval(timer);
+        }
+      }, 25 * Math.random());
+    });
+  }
+
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains("ativo")) {
+      animaNumeros();
+      observer.disconnect();
+    }
+  }
+
+  const observer = new MutationObserver(handleMutation);
+  const observerTarget = document.querySelector(".numeros");
+
+  observer.observe(observerTarget, { attributes: true });
+}
