aa2b099980ec83c90ca531d6d923ce2901f42e04
Filipe <fgallo_dev@hotmail.com>
2022-10-24 13:46:02 -0300

    Button Creator

---
+++ efeitos-no-dom/button-creator/script.js
@@ -0,0 +1,49 @@
+const controles = document.querySelector("#controles");
+const cssText = document.querySelector(".css");
+const btn = document.querySelector(".btn");
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + "px";
+  },
+  width(value) {
+    this.element.style.width = value + "px";
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + "px";
+  },
+  texto(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + "rem";
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+};
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+  showCss();
+}
+
+function showCss() {
+  cssText.innerHTML =
+    "<span>" + btn.style.cssText.split("; ").join("; </span><span>");
+}
+
+controles.addEventListener("change", handleChange);
