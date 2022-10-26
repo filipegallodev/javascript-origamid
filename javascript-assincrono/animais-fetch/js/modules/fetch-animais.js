bb4bc64e102e32e15a02cb1a05896c381f293a38
Filipe <fgallo_dev@hotmail.com>
2022-10-26 09:35:41 -0300

    Animais Fetch

---
+++ javascript-assincrono/animais-fetch/js/modules/fetch-animais.js
@@ -0,0 +1,29 @@
+import { initAnimaNumeros } from "./anima-numeros.js";
+
+export function initFetchAnimais() {
+  async function fetchAnimais(url) {
+    const animaisResponse = await fetch(url);
+    const animaisJson = await animaisResponse.json();
+    const numerosGrid = document.querySelector(".numeros-grid");
+
+    animaisJson.forEach((animal) => {
+      const divAnimal = createAnimal(animal);
+      numerosGrid.appendChild(divAnimal);
+    });
+
+    initAnimaNumeros();
+  }
+
+  function createAnimal(animal) {
+    const div = document.createElement("div");
+    div.classList.add("numero-animal");
+    div.innerHTML = `
+  <h3>${animal.specie}</h3>
+  <span data-numero>${animal.total}</span>
+  `;
+
+    return div;
+  }
+
+  fetchAnimais("./animais-api.json");
+}
