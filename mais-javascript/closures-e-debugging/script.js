2e26a42fc5ec727a4eae434fd080cca10790cf8f
Filipe <fgallo_dev@hotmail.com>
2022-10-27 08:44:06 -0300

    Closures e Debugging

---
+++ mais-javascript/closures-e-debugging/script.js
@@ -0,0 +1,25 @@
+// let item1 = 1;
+// function funcao1() {
+//   let item2 = 2;
+//   function funcao2() {
+//     let item3 = 3;
+//     console.log(item1);
+//     console.log(item2);
+//     console.log(item3);
+//   }
+//   funcao2();
+// }
+// funcao1();
+
+function contagem() {
+  let total = 0;
+  return function incremento() {
+    total++;
+    console.log(total);
+  };
+}
+
+const ativarIncremento = contagem();
+ativarIncremento();
+ativarIncremento();
+ativarIncremento();
