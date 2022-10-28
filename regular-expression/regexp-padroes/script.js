be28e3f97d31f1793d6c027ca03977e7894370e1
Filipe <fgallo_dev@hotmail.com>
2022-10-28 10:22:14 -0300

    Regexp Padrões

---
+++ regular-expression/regexp-padroes/script.js
@@ -0,0 +1,111 @@
+const regexpCEP = /\d{5}[-\s]?\d{3}/g;
+
+const ceps = ["00000-000", "00000 000", "00000000"];
+
+for (cep of ceps) {
+  console.log(cep, cep.match(regexpCEP));
+}
+
+const regexpCPF = /(?:\d{3}[-.]?){3}\d{2}/g;
+
+const cpfs = [
+  "000.000.000-00",
+  "000-000-000-00",
+  "000.000.000.00",
+  "000000000-00",
+  "00000000000",
+];
+
+for (cpf of cpfs) {
+  console.log(cpf, cpf.match(regexpCPF));
+}
+
+const regexpCNPJ = /\d{2}[-.]?(?:\d{3}[-.]?){2}[-\/]?\d{4}[-.]?\d{2}/g;
+
+const cnpjs = [
+  "00.000.000/0000-00",
+  "00000000000000",
+  "00-000-000-0000-00",
+  "00.000.000/000000",
+  "00.000.000.000000",
+  "00.000.000.0000.00",
+];
+
+for (cnpj of cnpjs) {
+  console.log(cnpj, cnpj.match(regexpCNPJ));
+}
+
+const regexpTELEFONE = /(?:\+?55\s?)?(?:\(?\d{2}\)?[-\s]?)?\d{4,5}[-\s]?\d{4}/g;
+
+const telefones = [
+  "+55 11 98888-8888",
+  "+55 11 98888 8888",
+  "+55 11 988888888",
+  "+55 11988888888",
+  "+5511988888888",
+  "5511988888888",
+  "11 98888-8888",
+  "11 98888 8888",
+  "(11) 98888 8888",
+  "(11) 98888-8888",
+  "11-98888-8888",
+  "11 98888 8888",
+  "11988888888",
+  "11988888888",
+  "988888888",
+  "(11)988888888",
+  "98888 8888",
+  "8888 8888",
+];
+
+for (telefone of telefones) {
+  console.log(telefone, telefone.match(regexpTELEFONE));
+}
+
+const regexpEMAIL = /[\w.-]+@[\w-]+\.[\w-.]+/gi;
+
+const emails = [
+  "email@email.com",
+  "email@email.com.org",
+  "email-email@email.com",
+  "email_email@email.com",
+  "email.email23@email.com.br",
+  "email.email23@empresa-sua.com.br",
+  "c@contato.cc",
+];
+
+for (email of emails) {
+  console.log(email, email.match(regexpEMAIL));
+}
+
+const regexpTAGClasses = /<\/?[\w\s="']+\/?>/gi;
+
+const tags = [
+  "<div>Isso é uma div</div>",
+  '<div class="ativa">Essa está ativa</div>',
+  '<img src="imagem" />',
+  '<img src="imagem">',
+  '<ul class="ativa">',
+  "<li>Essa está ativa</li>",
+  "</ul>",
+];
+
+for (tag of tags) {
+  console.log(tag, tag.match(regexpTAGClasses));
+}
+
+const regexpTAG = /(?<=<\/?)[\w]+/gi;
+
+const tags2 = [
+  "<div>Isso é uma div</div>",
+  '<div class="ativa">Essa está ativa</div>',
+  '<img src="imagem" />',
+  '<img src="imagem">',
+  '<ul class="ativa">',
+  "<li>Essa está ativa</li>",
+  "</ul>",
+];
+
+for (tag of tags2) {
+  console.log(tag, tag.match(regexpTAG));
+}
