6e3d8feca0cdc832a945a7e598ad354c5fe8e54a
Filipe <fgallo_dev@hotmail.com>
2022-11-01 09:47:43 -0300

    Modal transformado em classe

--- projeto-final/animais-fantasticos/js/script.js
+++ projeto-final/animais-fantasticos/js/script.js
@@ -1,8 +1,8 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
-
 import TabNav from "./modules/tabnav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
+
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -23,7 +23,13 @@ const tabNav = new TabNav(
 );
 tabNav.init();
 
-initModal();
+const modal = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  '[data-modal="container"]'
+);
+modal.init();
+
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
