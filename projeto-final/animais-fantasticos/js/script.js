70efb9a82db7821ab510d55cde272d5d3786c81b
Filipe <fgallo_dev@hotmail.com>
2022-11-01 10:28:52 -0300

    Tooltip refatorada / transformada em classe

--- projeto-final/animais-fantasticos/js/script.js
+++ projeto-final/animais-fantasticos/js/script.js
@@ -2,8 +2,8 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabnav.js";
 import Modal from "./modules/modal.js";
+import Tooltip from "./modules/tooltip.js";
 
-import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
@@ -30,7 +30,9 @@ const modal = new Modal(
 );
 modal.init();
 
-initTooltip();
+const tooltip = new Tooltip("[data-tooltip]");
+tooltip.init();
+
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
