b7e6031924247f16ec67845dc3a2683b6bdcad41
Filipe <fgallo_dev@hotmail.com>
2022-11-01 11:27:27 -0300

    Mudança Usuário 2, adiciona console.log

--- projeto-final/animais-fantasticos/js/modules/fetch-bitcoin.js
+++ projeto-final/animais-fantasticos/js/modules/fetch-bitcoin.js
@@ -1,8 +1,10 @@
 export default function initFetchBitcoin() {
-  fetch('https://blockchain.info/ticker')
-    .then(response => response.json())
+  console.log("Teste");
+  fetch("https://blockchain.info/ticker")
+    .then((response) => response.json())
     .then((bitcoin) => {
-      const btcPreco = document.querySelector('.btc-preco');
+      const btcPreco = document.querySelector(".btc-preco");
       btcPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
-    }).catch(erro => console.log(Error(erro)));
+    })
+    .catch((erro) => console.log(Error(erro)));
 }
